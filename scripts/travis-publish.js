a68176a9c3b0f292172a74a7d6fb90fdbf5f3029
beajer <919060679@qq.com>
2020-08-31 10:36:18 +0800

    ci: test make

---
+++ scripts/travis-publish.js
@@ -0,0 +1,19 @@
+const shell = require("shelljs");
+const { version, name } = require("../package.json");
+
+shell.exec("npm run build");
+shell.cd("dist");
+shell.exec("npm i");
+shell.cd("../");
+
+const makePlatform = {
+  windows: "win64",
+  osx: "mac",
+  linux: "linux64",
+};
+shell.exec(`make ${makePlatform[process.platform]}`);
+shell.mkdir("-p", `releases/${version}/${process.platform}-x64`);
+shell.cp(
+  `build/${name}-${process.platform}-x64/resources / app.asar`,
+  `releases/${version}/${process.platform}-x64`
+);
